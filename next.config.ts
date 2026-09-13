026aa5fed26afadb44c421466b7b7361a36c2215
nodo gelashvili <166313791+gelashvilinodo@users.noreply.github.com>
2026-09-14 01:22:55 +0400

    fix: include pg cloudflare files in build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+
+  outputFileTracingIncludes: {
+    "**/*": [
+      "./node_modules/pg-cloudflare/dist/**",
+      "./node_modules/pg-cloudflare/esm/**",
+    ],
+  },
 };
 
 export default nextConfig;
